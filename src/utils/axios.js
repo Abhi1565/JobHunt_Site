963eb0999d6629e26b35aa37a466ead078a0b9a6
Abhijit Palve <palweabhijeet@gmail.com>
2025-07-18 13:10:26 +0530

    Adding new backend url to axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with default configuration
 const api = axios.create({
-  baseURL: 'https://jobhunt-backend-ttyn.onrender.com',
+  baseURL: 'https://jobhunt-backend-pddd.onrender.com',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
