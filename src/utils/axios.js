4ca17de96aca7a0fec2e594f9004354029d6ecc7
Abhijit Palve <palweabhijeet@gmail.com>
2025-07-19 23:24:33 +0530

    .

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with default configuration
 const api = axios.create({
-  baseURL: 'https://jobhunt-backend-es2k.onrender.com',
+  baseURL: 'https://jobhunt-backend-a3cc.onrender.com',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
