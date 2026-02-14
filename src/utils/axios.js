a9cb984158d5a8730b6e235b0d787ee4eb191068
Abhijit Palve <palweabhijeet@gmail.com>
2026-02-15 02:12:22 +0530

    Added env for axios to have flexible backend connectivity

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with default configuration
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
